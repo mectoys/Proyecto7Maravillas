12f414d5e532be2807f34fcb13bc8738eba8a86e
mectoys <mectoys2013@gmail.com>
2023-04-06 23:15:54 -0500

    Proyecto 7 Maravillas Mundo Moderno/Antiguo

--- scr/Main.py
+++ scr/Main.py
@@ -28,21 +28,18 @@ def MostrarMapaSieteMaravillas():
     with open('..\\data\\data.csv', 'r') as fmaravilla:
         readermaravilla = list(csv.reader(fmaravilla))
         # Bucle donde recorrermo maravilla moderna o Antigua
-        # print(ListDuplicate)
-        # print(list(dict.fromkeys(ListDuplicate) ))
         for maravilla in list(dict.fromkeys(ListDuplicate)):
             feature_group = folium.FeatureGroup(maravilla, show=False)
             datafiltrada = list(filter(lambda k: maravilla in k, readermaravilla))
             for filamaravilla in datafiltrada:
-
+                #Agrega el ícono
                 icon = folium.features.CustomIcon(filamaravilla[4], icon_size=(48,48))
-                #print(filamaravilla[1])
+                #Agregar popup que admite código HTML
                 htmlcode = """<div style="font-family: courier new; color: blue">
                                                  <img src={0} alt={1} width="230" height="172">
                                                  <br /><span><h5>{2}</h5></span> """.format(filamaravilla[5], filamaravilla[1],
                                                                                             filamaravilla[6])
                 feature_group.add_child(
-
                     folium.Marker( [float(filamaravilla[2] ),float(filamaravilla[3])],popup=htmlcode
                                    ,tooltip=filamaravilla[1],icon=icon))
             feature_group.add_to(m)
